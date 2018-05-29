9f7adb7a38b46081228ffde17684662a5a7b497f
franco-extreme <franck_ral@hotmail.com>
2018-05-28 22:26:39 -0400

    uo-social-service: Created the initial structure of the project.

---
+++ src/main/java/com/umssonline/social/uosocialservice/UoSocialServiceApplication.java
@@ -0,0 +1,12 @@
+package com.umssonline.social.uosocialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoSocialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoSocialServiceApplication.class, args);
+	}
+}
