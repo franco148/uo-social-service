148f767acabf9c91b92fdda105bf22b269913385
franco-extreme <franck_ral@hotmail.com>
2018-06-06 19:59:00 -0400

    SocialService-ImplementMessagesProcess: Added required DTOs for messages process.

---
+++ src/main/java/com/umssonline/social/common/dto/CreateMessageDto.java
@@ -0,0 +1,22 @@
+package com.umssonline.social.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CreateMessageDto {
+
+    private String text;
+    private Long createdById;
+
+    @JsonIgnore
+    private Boolean isDeleted = false;
+    @JsonIgnore
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @JsonIgnore
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
