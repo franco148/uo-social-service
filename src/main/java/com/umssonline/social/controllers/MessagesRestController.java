c0dbebcfd65828e7895719a5ceb8510e4329a4d6
franco-extreme <franck_ral@hotmail.com>
2018-06-10 17:54:14 -0400

    SocialService-ImplementMessagesProcess: Integrated Controller layer in the process.

--- src/main/java/com/umssonline/social/controllers/MessagesRestController.java
+++ src/main/java/com/umssonline/social/controllers/MessagesRestController.java
@@ -1,7 +1,7 @@
 package com.umssonline.social.controllers;
 
-import com.umssonline.social.common.dto.CreateCommentDto;
-import com.umssonline.social.common.dto.UpdateCommentDto;
+import com.umssonline.social.common.dto.CreateMessageDto;
+import com.umssonline.social.common.dto.UpdateMessageDto;
 import com.umssonline.social.models.Message;
 import org.springframework.http.ResponseEntity;
 
@@ -9,15 +9,15 @@ import java.util.Collection;
 
 public interface MessagesRestController {
 
-    ResponseEntity<Message> findById(final Long id);
+    ResponseEntity<Message> findById(final Long id) throws Exception;
 
-    ResponseEntity<Collection<Message>> findAll();
+    ResponseEntity<Collection<Message>> findAll() throws Exception;
 
     ResponseEntity<Collection<Message>> findByProperty(final String property, final Object value);
 
-    ResponseEntity<Message> create(final CreateCommentDto commentDto);
+    ResponseEntity<Message> create(final CreateMessageDto messageDto) throws Exception;
 
-    ResponseEntity<Message> update(final UpdateCommentDto updateCommentDto);
+    ResponseEntity<Message> update(final UpdateMessageDto messageDto) throws Exception;
 
     ResponseEntity<Void> deleteById(final Long id);
 }
