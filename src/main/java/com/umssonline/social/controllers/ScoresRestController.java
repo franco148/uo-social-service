4ccba49cdd191d382cfadad6b92641434f47ce14
franco-extreme <franck_ral@hotmail.com>
2018-06-22 22:57:11 -0400

    SocialService-ImmplementRatesProcess: Implemented the Controllers Layer for Scores feature.

--- src/main/java/com/umssonline/social/controllers/ScoresRestController.java
+++ src/main/java/com/umssonline/social/controllers/ScoresRestController.java
@@ -7,13 +7,13 @@ import java.util.Collection;
 
 public interface ScoresRestController {
 
-    ResponseEntity<Score> findById(final Long id);
+    ResponseEntity<Score> findById(final Long id) throws Exception;
 
-    ResponseEntity<Collection<Score>> findAll();
+    ResponseEntity<Collection<Score>> findAll() throws Exception;
 
     ResponseEntity<Collection<Score>> findByProperty(final String propName, final  String propValue);
 
-    ResponseEntity<Score> create(final Object scoreDto);
+    ResponseEntity<Score> create(final Object scoreDto) throws Exception;
 
     ResponseEntity<Score> update(final Object scoreDto);
 
--- src/main/java/com/umssonline/social/controllers/ScoresRestControllerImpl.java
+++ src/main/java/com/umssonline/social/controllers/ScoresRestControllerImpl.java
@@ -4,9 +4,9 @@ import com.umssonline.social.models.Score;
 import com.umssonline.social.services.SocialService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -24,33 +24,65 @@ public class ScoresRestControllerImpl implements ScoresRestController {
 
     //region ScoresRestController Members
     @Override
-    public ResponseEntity<Score> findById(Long id) {
-        return null;
+    @GetMapping("/{score_id}")
+    public ResponseEntity<Score> findById(@PathVariable("score_id") final Long id) throws Exception {
+
+        try {
+            Score scoreResponse = scoreService.findById(id);
+            return new ResponseEntity<>(scoreResponse, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw new Exception("Error on getting a score: " + ex.getMessage());
+        }
     }
 
     @Override
-    public ResponseEntity<Collection<Score>> findAll() {
-        return null;
+    @GetMapping
+    public ResponseEntity<Collection<Score>> findAll() throws Exception {
+
+        try {
+            Collection<Score> scoresResponse = scoreService.findAll();
+
+            if (scoresResponse.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(scoresResponse, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw new Exception("Error on getting scores: " + ex.getMessage());
+        }
     }
 
     @Override
+    @GetMapping("/property")
     public ResponseEntity<Collection<Score>> findByProperty(String propName, String propValue) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Score> create(Object scoreDto) {
+    @PostMapping
+    public ResponseEntity<Score> create(@RequestBody final Object scoreDto) throws Exception {
+
+        Score converted = modelMapper.map(scoreDto, Score.class);
+        try {
+            Score savedScore = scoreService.save(converted);
+            return new ResponseEntity<>(savedScore, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            throw new Exception("Save score has failed. The operation has been terminated: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    @PutMapping
+    public ResponseEntity<Score> update(@RequestBody final Object scoreDto) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Score> update(Object scoreDto) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteById(Long id) {
-        return null;
+    @DeleteMapping("/{score_id}")
+    public ResponseEntity<Void> deleteById(@PathVariable("score_id") final Long id) {
+        scoreService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     //endregion
 }
