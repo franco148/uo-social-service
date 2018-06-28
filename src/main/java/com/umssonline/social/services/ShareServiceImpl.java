fa545e8f1b0b3508831e2f96ea95fe5ee707c19a
franco-extreme <franck_ral@hotmail.com>
2018-06-27 23:10:18 -0400

    SocialService-ImplementRatesProcess: Implemented Share service implementation.

--- src/main/java/com/umssonline/social/services/ShareServiceImpl.java
+++ src/main/java/com/umssonline/social/services/ShareServiceImpl.java
@@ -1,5 +1,7 @@
 package com.umssonline.social.services;
 
+import com.umssonline.social.models.Participant;
+import com.umssonline.social.models.Resource;
 import com.umssonline.social.models.Share;
 import com.umssonline.social.repositories.api.ExtendedShareDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,18 @@ public class ShareServiceImpl implements SocialService<Share> {
     //region Properties
     @Autowired
     private ExtendedShareDao shareDao;
+
+    @Autowired
+    private SocialService<Resource> resourceService;
+
+    @Autowired
+    private SocialService<Participant> participantService;
     //endregion
 
     //region SocialService Members
     @Override
     public Share findById(Serializable id) throws Exception {
-        return null;
+        return shareDao.findById(id);
     }
 
     @Override
@@ -29,12 +37,30 @@ public class ShareServiceImpl implements SocialService<Share> {
 
     @Override
     public Collection<Share> findAll() {
-        return null;
+        return shareDao.findAll();
     }
 
     @Override
     public Share save(Share share) throws Exception {
-        return null;
+
+        if (share.getSharedResource() == null || share.getCreatedBy() == null) {
+            throw new Exception("Neither Resource nor CreatedBy properties can be null.");
+        }
+
+        Resource resourceFromDb = resourceService.findById(share.getSharedResource().getId());
+        if (resourceFromDb == null) {
+            throw new Exception("Share can not be created, it does not have a related Resource");
+        }
+
+        Participant participantFromDb = participantService.findById(share.getCreatedBy().getId());
+        if (participantFromDb == null) {
+            throw new Exception("The owner who is creating the share does not exists.");
+        }
+
+        share.setSharedResource(resourceFromDb);
+        share.setCreatedBy(participantFromDb);
+
+        return shareDao.create(share);
     }
 
     @Override
@@ -44,12 +70,12 @@ public class ShareServiceImpl implements SocialService<Share> {
 
     @Override
     public void delete(Share share) {
-
+        shareDao.deleteById(share.getId());
     }
 
     @Override
     public void deleteById(Serializable id) {
-
+        shareDao.deleteById(id);
     }
     //endregion
 }
