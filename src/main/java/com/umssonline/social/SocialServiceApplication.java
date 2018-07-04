1f86ffec899e11d7eab677c04ac29a8e4123da79
franco-extreme <franck_ral@hotmail.com>
2018-07-03 23:11:03 -0400

    SocialService-RestControllers: Updated main class of the project.

--- src/main/java/com/umssonline/social/SocialServiceApplication.java
+++ src/main/java/com/umssonline/social/SocialServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UoSocialServiceApplication {
+public class SocialServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UoSocialServiceApplication.class, args);
+		SpringApplication.run(SocialServiceApplication.class, args);
 	}
 }
